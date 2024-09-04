789fd45ae877851853814413d13335aa6daf4a88
Nikita Skazki <nskazki@gmail.com>
2024-09-04 13:17:50 +1000

    Draw text without delay

--- draw/draw.py
+++ draw/draw.py
@@ -17,21 +17,30 @@ sys.path.append(libdir)
 
 from waveshare_epd import epd2in13_V4
 
+TRUNCAT_AT = 18
+IDLE_DELAY = 5
+CLEAR_DELAY = 3
+TIMER_INTERVAL = 0.25
+FRAME_INTERVAL = 0.5
+
 epd = epd2in13_V4.EPD()
 font = ImageFont.truetype(os.path.join(assets, "Sevillana.ttf"), 32)
 
-ip = None
-bt = None
-idle_at = None
+step_at = None
+clear_at = None
 updated_at = datetime.now()
-initialized = False
+
+current_bt = None
+current_ip = None
 current_text = None
 
-walk_step = 1
-walk_limit = 5
+step_count = 8
+current_step = 0
 
+frame_event = threading.Event()
 timer_event = threading.Event()
-display_event = threading.Event()
+
+display_ready = False
 
 def spawn(target):
     thread = threading.Thread(target=target)
@@ -40,44 +49,53 @@ def spawn(target):
     return thread
 
 def manage_timer():
-    global idle_at, updated_at, current_text
-    timer_event.clear()
-    if idle_at:
-        if idle_at >= datetime.now():
-            print("Idling by timer")
-            idle_at = None
-            updated_at = datetime.now()
-            current_text = None
-            display_event.set()
-        else:
-            sleep(10)
-            manage_timer()
-    else:
-        timer_event.wait()
-        manage_timer()
+    while True:
+        try:
+            timer_event.clear()
+
+            if is_past(clear_at):
+                print("Clearing by timer")
+                new_text(None)
+                new_clear_at(None)
+
+            if is_past(step_at):
+                print("Incrementing the step")
+                new_step(current_step + 1)
+                new_step_at(None)
+
+            if clear_at or step_at:
+                sleep(TIMER_INTERVAL)
+            else:
+                timer_event.wait()
+        except Exception as e:
+            print(f"An unexpected error occurred in the timer manager: {e}")
+            traceback.print_exc()
 
 def manage_frame():
-    global walk_step
-    display_event.clear()
+    while True:
+        try:
+            frame_event.clear()
 
-    text = current_text or bt or ip or "You are beautiful"
-    if len(text) >= 20:
-        text = text[:17] + "..."
+            text = current_text or current_bt or current_ip or "You are beautiful"
+            text = truncate(text)
 
-    if updated_at <= seconds_ago(30) and not current_text:
-        print("Have been idle for a while")
-        draw_display(with_text(os.path.join(assets, "rina.bmp"), text))
-        freeze_display()
-        display_event.wait()
-        manage_frame()
-    else:
-        print(f"Animating {walk_step}")
-        draw_display(with_text(os.path.join(assets, "cats", f"{walk_step}.bmp"), text))
-        walk_step += 1
-        if walk_step > walk_limit:
-            walk_step = 1
-        sleep(0.5)
-        manage_frame()
+            if not current_text and is_older_than(updated_at, IDLE_DELAY):
+                print("Have been idle for a while")
+                draw_display(with_text("rina.bmp", text))
+                freeze_display()
+            else:
+                new_step_at(FRAME_INTERVAL)
+                animation = (current_step % step_count) + 1
+                draw_display(with_text(f"cats/{animation}.bmp", text))
+                if not current_text:
+                    freeze_in = updated_at - seconds_ago(IDLE_DELAY)
+                    if freeze_in >= timedelta(seconds=0):
+                        print(f"Freeze in {freeze_in}")
+
+            frame_event.wait()
+        except Exception as e:
+            print(f"An unexpected error occurred in the frame manager: {e}")
+            traceback.print_exc()
 
 def listen_to_stdin():
     while True:
@@ -85,7 +103,7 @@ def listen_to_stdin():
             input = sys.stdin.readline().strip()
             process_lines(input)
         except Exception as e:
-            print(f"An unexpected error occurred: {e}")
+            print(f"An unexpected error occurred in the stdin listener: {e}")
             traceback.print_exc()
 
 def listen_to_socket():
@@ -102,7 +120,7 @@ def listen_to_socket():
                     print(f"Processing {input}")
                     process_lines(input)
                 except Exception as e:
-                    print(f"An unexpected error occurred: {e}")
+                    print(f"An unexpected error occurred in the socket listener: {e}")
                     traceback.print_exc()
             else:
                 print(f"Closing the connection")
@@ -123,17 +141,17 @@ def process_lines(lines):
         process_line(line)
 
 def process_line(line):
-    target, value = parse_line(line)
+    what, value = parse_line(line)
     if line.startswith("Flush:"):
         plan_flush(value)
     elif line.startswith("Draw:"):
         plan_draw(value)
-    elif line.startswith("Idle!"):
-        plan_idle()
+    elif line.startswith("Clear!"):
+        plan_clear()
     elif line.startswith("IP:"):
-        plan_stat_update(target, value)
+        plan_stat_update(what, value)
     elif line.startswith("BT:"):
-        plan_stat_update(target, value)
+        plan_stat_update(what, value)
     else:
         plan_draw(line)
 
@@ -145,40 +163,61 @@ def parse_line(input):
         return [None, None]
 
 def plan_flush(value):
-    global idle_at, updated_at, current_text
-    idle_at = seconds_from_now(30)
-    updated_at = datetime.now()
-    current_text = value
-    set_events()
+    new_text(value)
+    new_clear_at(CLEAR_DELAY)
 
 def plan_draw(value):
-    global idle_at, updated_at, current_text
-    idle_at = None
-    updated_at = datetime.now()
-    current_text = value
-    set_events()
+    new_text(value)
+    new_clear_at(None)
 
-def plan_idle():
-    global idle_at, updated_at, current_text
-    idle_at = None
-    updated_at = datetime.now()
-    current_text = None
-    set_events()
+def plan_clear():
+    new_text(None)
+    new_clear_at(None)
 
 def plan_stat_update(what, value):
-    global ip, bt, updated_at
-    updated_at = datetime.now()
-
     if what == "IP":
-        ip = value
+        new_ip(value)
     elif what == "BT":
-        bt = value
+        new_bt(value)
 
-    set_events()
+def new_ip(value):
+    global updated_at, current_ip
+    current_ip = value
+    updated_at = datetime.now()
+    frame_event.set()
 
-def set_events():
+def new_bt(value):
+    global updated_at, current_bt
+    current_bt = value
+    updated_at = datetime.now()
+    frame_event.set()
+
+def new_step(value):
+    global current_step
+    current_step = value
+    frame_event.set()
+
+def new_text(value):
+    global updated_at, current_text
+    updated_at = datetime.now()
+    current_text = value
+    frame_event.set()
+
+def new_step_at(seconds):
+    global step_at
+    if seconds:
+        step_at = seconds_from_now(seconds)
+    else:
+        step_at = None
     timer_event.set()
-    display_event.set()
+
+def new_clear_at(seconds):
+    global clear_at
+    if seconds:
+        clear_at = seconds_from_now(seconds)
+    else:
+        clear_at = None
+    timer_event.set()
 
 def seconds_ago(seconds):
     return datetime.now() - timedelta(seconds=seconds)
@@ -187,35 +226,48 @@ def seconds_from_now(seconds):
     return datetime.now() + timedelta(seconds=seconds)
 
 def draw_display(image):
-    if initialized:
-        print("Drawing on display")
+    if display_ready:
         epd.displayPartial(epd.getbuffer(image))
     else:
         init_display(image)
 
 def init_display(image):
-    global initialized
+    global display_ready
     print("Initalizing display")
-    initialized = True
+    display_ready = True
     epd.init()
     epd.displayPartBaseImage(epd.getbuffer(image))
 
 def freeze_display():
-    global initialized
+    global display_ready
     print("Freezing display")
-    initialized = False
+    display_ready = False
     epd.sleep()
 
 def halt_display():
     print("Exiting the EPD module")
+    epd.sleep()
     epd2in13_V4.epdconfig.module_exit(cleanup=True)
 
-def with_text(path, text):
+def with_text(asset_path, text):
+    path = os.path.join(assets, asset_path)
     base = Image.new("1", (250, 122), 255)
     base.paste(Image.open(path), (0, 0))
     draw = ImageDraw.Draw(base)
     draw.text((0, 0), text, font=font, fill=0)
     return base
+
+def is_past(time):
+    return time and time <= datetime.now()
+
+def is_older_than(time, seconds):
+    return time and time <= seconds_ago(seconds)
+
+def truncate(text):
+    if len(text) >= TRUNCAT_AT:
+        return text[:TRUNCAT_AT] + "..."
+    else:
+        return text
 
 atexit.register(halt_display)
 
